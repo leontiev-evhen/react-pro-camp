e33afc78018559a955231b0fae9ab62d0025c911
Yevhen Leontiev <leo@mail.cz>
2019-07-01 17:53:00 +0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import { Header } from './components';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
-function App() {
-	return (
-		<React.Fragment>
-			<Header />
-			<main>
-				<Container>
-					<Row>
-						<Col>1 Section</Col>
-						<Col>2 Section</Col>
-						<Col>3 Section</Col>
-					</Row>
-				</Container>
-			</main>
-		</React.Fragment>
-	);
-}
+const App = () => (
+	<Router>
+		<Header />
+		<main>
+			<Container>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route component={NotFound} />
+				</Switch>
+			</Container>
+		</main>
+	</Router>
+);
 
 export default App;
