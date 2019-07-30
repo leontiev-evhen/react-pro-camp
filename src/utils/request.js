c6d370419fb5b67345ccd977ef936258b4a6fef2
Yevhen Leontiev <leontevevgenii@gmail.com>
2019-07-30 11:13:53 +0300

    remove all console statements

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,21 +15,10 @@ const request = async function(options) {
 		if (response.data.api.results === 0) {
 			return Promise.reject({ message: 'Nothing was found' });
 		}
-		console.info('Request Successful!', response);
 		return response.data;
 	};
 
 	const onError = function(error) {
-		console.error('Request Failed:', error.config);
-
-		if (error.response) {
-			console.error('Status:', error.response.status);
-			console.error('Data:', error.response.data);
-			console.error('Headers:', error.response.headers);
-		} else {
-			console.error('Error Message:', error.message);
-		}
-
 		return Promise.reject(error.response || error.message);
 	};
 
